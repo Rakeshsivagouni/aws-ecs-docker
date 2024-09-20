c9b9d2068e1f57b7b7d846f0a5e84a856fdbf9f7
rakeshsivagouni <rakesh.sivagouni@gmail.com>
2024-09-20 09:53:57 +0000

    port changed

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/info", (req, res) => {
   res.send("hey this is /info api");
 });
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(8000, () => {
+  console.log("listening on 8000");
 });
